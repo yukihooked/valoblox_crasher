d6aadd39ce865d0733764d4e19c87ae7df14aab3
YUKINO <78239937+yukihooked@users.noreply.github.com>
2022-07-05 06:25:52 -0400

    Update source.lua

--- source.lua
+++ source.lua
@@ -2,7 +2,7 @@ local LocalPlayer = game.Players.LocalPlayer
 
 while task.wait(1) do
     for i = 1, 1900 do -- Maximum allowed remote calls per second is 1900-2000 before roblox itself kicks u
-        if LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("CLASSIC") then
+        if LocalPlayer.Character and LocalPlayer.Character:FindFirstChildOfClass("Tool") then
             LocalPlayer.Character:FindFirstChildOfClass("Tool").UnrealEngine.FireSound:FireServer(workspace.Map)
         end
     end
